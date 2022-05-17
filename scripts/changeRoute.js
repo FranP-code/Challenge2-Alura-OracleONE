fb522a6d9d032436a164c2bf56e0dd0f2230d4de
Francisco Pessano <franpessano1@gmail.com>
2022-05-17 17:24:18 -0300

    Maquetación de la ruta add-word lista

--- scripts/changeRoute.js
+++ scripts/changeRoute.js
@@ -14,14 +14,16 @@ function changeRoute(route) {
         
         case "add-word": 
             mainElement.innerHTML = `
-                <input id="add-word-input">
+                <textarea id="add-word-input" type="text" maxlength="8" placeholder="Ingrese la nueva palabra aquí"></textarea>
                 <div class="warning">
-                    <img src="/img/warning.png" alt="warning">
+                    <img src="/img/warning.svg" alt="warning">
                     Max. 8 carácteres
                 </div>
-                <button class="main">Guardar y empezar</button>        
-                <button onclick="changeRoute('base')">Cancelar</button>        
-            `
+                <div class="buttons-container">
+                    <button class="main">Guardar y empezar</button>        
+                    <button onclick="changeRoute('base')">Cancelar</button>        
+                </div>
+                `
             break
 
         case "game":
